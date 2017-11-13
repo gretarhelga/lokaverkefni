d98e6476c7bdaccd0ae75b555738a53ffee509d3
gretar <gretar95@gmail.com>
2017-11-13 15:18:50 +0000

    lokaverkefni

--- resources/views/shoes/index.blade.php
+++ resources/views/shoes/index.blade.php
@@ -7,7 +7,7 @@
             <article>
                 <div class="row">
                     @foreach($chunk as $shoe)
-                    <a href="/shoes/{{ $shoe->id }}" style="text-decoration: none;">
+                    <a href="/shoes/{{ $shoe->id }}" style="color: black;">
                         <div class="col-xs-3" style="border: 1px solid black;">
                             <div >{{ $shoe->brand }}</div>
                             <div>{{ $shoe->color }}</div>
--- resources/views/shoes/show.blade.php
+++ resources/views/shoes/show.blade.php
@@ -2,7 +2,19 @@
 
 @section('content')
 
-<h1>{{ $shoes->id }}</h1>
-<h1>{{ $shoes->brand }}</h1>
+
+
+<div class="row">
+  <div class="col-xs-5 col-xs-offset-2" style="border: 1px solid black;">
+  	<img src="/image/mynd.jpg" style="width: 100%;">
+  </div>
+
+  <div class="col-xs-3" style="border: 1px solid black;">
+  	<h2>{{ $shoes->brand }}</h2>
+  	<h4>Size: {{ $shoes->size }}</h4>
+  	<h4>{{ $shoes->price }} ISK</h4>
+  </div>
+</div>
+
 
 @endsection
