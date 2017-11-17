8579b57473265373ad2a2630162e4f6c7e2f5eba
gretar <gretar95@gmail.com>
2017-11-17 10:09:47 +0000

    lokaverkefni

--- resources/views/shoes/index.blade.php
+++ resources/views/shoes/index.blade.php
@@ -9,6 +9,7 @@
                     @foreach($chunk as $shoe)
                     <a href="/shoes/{{ $shoe->id }}" style="color: black;">
                         <div class="col-xs-3" style="border: 1px solid black;">
+                            <img src="https://cdn.pixabay.com/photo/2013/07/12/18/20/chucks-153310_960_720.png" style="width: 100%;" >
                             <div >{{ $shoe->brand }}</div>
                             <div>{{ $shoe->color }}</div>
                         </div>
--- resources/views/shoes/show.blade.php
+++ resources/views/shoes/show.blade.php
@@ -6,7 +6,7 @@
 
 <div class="row">
   <div class="col-xs-5 col-xs-offset-2" style="border: 1px solid black;">
-  	<img src="/image/mynd.jpg" style="width: 100%;">
+  	<img src="https://cdn.pixabay.com/photo/2013/07/12/18/20/chucks-153310_960_720.png" style="width: 100%;">
   </div>
 
   <div class="col-xs-3" style="border: 1px solid black;">
